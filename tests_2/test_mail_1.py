23eecab76e6a8ca124a6b0f1269bb2f0a97a49bc
shopi <shopin_va@mail.ru>
2022-10-17 21:14:27 +0700

    create test_2 folder with new tests

---
+++ tests_2/test_mail_1.py
@@ -0,0 +1,6 @@
+def test_sending_mail_1(set_up, some):
+    print('письмо отправлено')
+
+
+def test_sending_mail_2(set_up, some):
+    print('письмо отправлено')
---
+++ tests_2/test_mail_2.py
@@ -0,0 +1,6 @@
+def test_sending_mail_3(set_up):
+    print('письмо отправлено')
+
+
+def test_sending_mail_4(set_up):
+    print('письмо отправлено')
