6c8809e91fd039105f4a2495cede38b857403c99
shopi <shopin_va@mail.ru>
2023-04-28 21:31:17 +0700

    modern test

--- src/base_classes/response.py
+++ src/base_classes/response.py
@@ -6,20 +6,27 @@ class Response:
 
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get('data')
         self.response_status = response.status_code
+        self.parsed_object = None
 
     def validate(self, schema):
-        if isinstance(self.response.json(), list):
-            for item in self.response.json():
-                schema.parse_obj(item)
+        if isinstance(self.response_json, list):
+            for item in self.response_json:
+                parsed_object = schema.parse_obj(item)
+                self.parsed_object = parsed_object
         else:
-            schema.parse_obj(self.response.json)
-        return self
+            schema.parse_obj(self.response_json)
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status in status_code, self
         else:
-            assert self.response_status == status_code, GlobalErrorMessages.WRONG_STATUS_CODE.value
+            assert self.response_status == status_code, self
         return self
+
+    def __str__(self):
+        return \
+            f'\n Status code: {self.response_status} \n ' \
+            f'Requested url: {self.response.url} \n ' \
+            f'Response body: {self.response_json}'
