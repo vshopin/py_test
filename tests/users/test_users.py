5faa1cb5e25933ee1abbc63c5a93d9aaa9898e75
shopi <shopin_va@mail.ru>
2023-04-29 20:47:01 +0700

    add allure

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -9,10 +9,14 @@ def test_getting_users_list(get_users):
     test_obj.assert_status_code(200).validate(User)
 
 
+@pytest.mark.development
 @pytest.mark.production
 @pytest.mark.skip
 def test_another():
-    assert 1 == 1
+    """
+    In that test we are try to check that 1 is equal 2
+    """
+    assert 1 == 2
 
 
 @pytest.mark.development
@@ -24,4 +28,7 @@ def test_another():
     ('b', 'b', 'bb')
 ])
 def test_calculation(first_value, second_value, result, calculate):
+    """
+    In that test we are testing calculating with different values(Valid and invalid)
+    """
     assert calculate(first_value, second_value) == result
